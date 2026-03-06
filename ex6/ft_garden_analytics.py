edb86796c5cee230a668c34a308730bf7d50e214
anjakob <anjakob@student.42heilbronn.de>
2026-03-06 23:25:05 +0100

    prototype. Idk what 42 wants form me. Subject is very confusing...

---
+++ ex6/ft_garden_analytics.py
@@ -0,0 +1,35 @@
+class GardenManager:
+    
+    def __init__(self) -> None:
+        self.owners = []
+
+    @staticmethod
+    def GardenStats() -> None:
+    
+    @classmethod
+    def create_garden_network(cls) -> None:
+    
+    def add_to_garden(cls, name_of_garden)
+        if name_of_garden == cls.owners # Owners of garden list
+            # I guess inherit
+
+class Plant:
+    def __init__(self, name: str = None, height: int = None, age: int = None) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+class FloweringPlant(Plant):
+
+class PrizeFlower(FloweringPlant):
+
+def ft_garden_analytics() -> None:
+    print("=== Garden Management System Demo ===")
+    print()
+    manager = GardenManager("Alice")
+
+    print("=== Alice's Garden Report ===")
+    alice_garden.GardenStats()
+
+if __name__ = "__main__":
+    ft_garden_analytics()
